4b2984e1dbdea63d6c278fc87c429bb901a32fce
Scott Miller <scottc.miller@live.com>
2021-07-25 18:20:35 -0700

    Update ViewCommand.java

--- ViewCommand.java
+++ ViewCommand.java
@@ -29,13 +29,12 @@ import me.scmiller.guildarmory.SingletonConnection;
  * 
  * TODO: Link Guild's Armory Google Sheet in Title when fully integrated.
  * 
- * @author &#064scottc_miller
  */
 
 public class ViewCommand extends Command {
-	
-	public ViewCommand() {
-		
+
+	public ViewCommand() 
+	{
 		super.name      = "View";
 		super.help      = "Returns armory info on a character";
 		super.arguments = "[character's name]";
